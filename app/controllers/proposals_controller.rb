5194f6b94f268e308561535d1b7d5bce01f9c5cb
ouin85 <bouinten@yahoo.fr>
2020-06-09 19:02:15 +0200

    install active storage

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,4 +1,11 @@
 class ProposalsController < ApplicationController
   def index
   end
+
+  def show
+    puts '$'*110
+    p params
+    puts '$'*110
+    # @proposal = Proposal.find(params[:id])
+  end
 end
--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,9 +1,13 @@
 class Proposal < ApplicationRecord
+  # Relations
   belongs_to :city
   belongs_to :category
   belongs_to :user
   has_many :comments
   has_many :votes
+
+  # Active storage
+  # has_one_attached :picture
 
   # Instance methods
   def comments_count
