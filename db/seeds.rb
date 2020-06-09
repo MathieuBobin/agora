6d908939ff35d3006f1324c5bd73ad1f5ae6056f
Mathieu <bobin.mathieu.f@gmail.com>
2020-06-09 15:31:59 +0200

    fichier seeds V1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+City.destroy_all
+puts "Les villes ont été détruites"
+User.destroy_all
+puts "Les utilisateurs ont été détruits"
+
+
+City.create!(name: "Paris", zip_code: "75000")
+City.create!(name: "Lyon", zip_code: "69000")
+City.create!(name: "Marseille", zip_code: "13000")
+City.create!(name: "Bordeaux", zip_code: "33000")
+
+puts "Les villes ont été créées"
+
+10.times do 
+  User.create!(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name,
+    city: City.first.id,
+    email: Faker::Internet.email,
+    encrypted_password: "coucou"
+  )
+end
+
+puts "Les utilisateurs ont été créées"
+
+
+
