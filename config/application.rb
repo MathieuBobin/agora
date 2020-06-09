e87dbf6e39e1ff5c5d157fb18865aa3538b4ed1f
mathildy <mathilde.prouchandy@gmail.com>
2020-06-09 15:12:56 +0200

    translating devise messages to french

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module Agora
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    I18n.enforce_available_locales = false
+
+    I18n.config.available_locales = :fr
+    config.i18n.default_locale = :fr
+    config.time_zone = 'Europe/Paris'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
