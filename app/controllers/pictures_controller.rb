4997048c472f1eb8b60532dd899a999e0525a300
ouin85 <bouinten@yahoo.fr>
2020-06-10 17:12:49 +0200

    test active storage

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,6 +2,6 @@ class PicturesController < ApplicationController
   def create
     @proposal = Proposal.find(params[:proposal_id])
     @proposal.picture.attach(params[:picture])
-    redirect_to proposal_path @proposal
+    redirect_to @proposal
   end
 end
