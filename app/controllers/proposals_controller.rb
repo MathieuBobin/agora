5940f37f14e6578a491feb00d1dcc2e1de5268f7
Mathieu <bobin.mathieu.f@gmail.com>
2020-06-10 17:46:38 +0200

    homepage actualisée

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,4 +1,9 @@
 class ProposalsController < ApplicationController
   def index
+    @paris = City.find(1)
+    @proposal_paris= @paris.proposals.sort { |p1, p2| p2.votes_count <=> p1.votes_count }.first(5)
+
+    @lyon = City.find(2)
+    @proposal_lyon= @lyon.proposals.sort { |p1, p2| p2.votes_count <=> p1.votes_count }.first(5)
   end
 end
