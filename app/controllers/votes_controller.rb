82ba41817afa452910fd1d274c6df56db817f2bd
ouin85 <bouinten@yahoo.fr>
2020-06-13 15:48:41 +0200

    add before_action method : not_permit_to_vote_in_other_cities and redirect to current_user city

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,35 +1,39 @@
 class VotesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :not_permit_to_vote_in_other_cities, only: :create
   
   def create
-    @city = Proposal.find(permitted_proposal_id_param).city
-    if current_user.city.id != Proposal.find(params[:proposal_id]).city_id
-      flash[:alert] = "Vous ne pouvez pas voter pour les propositions en dehors de votre ville! ;)"
-      redirect_to @city
-    else
-      Vote.create!(
-      user_id: current_user.id,
+    Vote.create!(
+      user: current_user,
       proposal_id: permitted_proposal_id_param
     )
-    flash[:success] = "Votre vote a bien été pris en compte! ;)"  
+    
+    flash[:success] = "Votre vote a bien été pris en compte !"  
     redirect_back fallback_location: root_path
-    end
   end
-
+  
   def destroy
     Vote.destroy(permitted_vote_id_param)
-
+    
     @city = Proposal.find(params[:proposal_id]).city
     redirect_back fallback_location: root_path
   end
-
+  
   private
-
+  
   def permitted_proposal_id_param
     params.permit(:proposal_id).require(:proposal_id)
   end
-
+  
   def permitted_vote_id_param
     params.permit(:id).require(:id)
+  end
+  
+  def not_permit_to_vote_in_other_cities
+    proposal = Proposal.find(permitted_proposal_id_param)
+    unless proposal.city.id == current_user.city.id
+      flash[:alert] = "Vous ne pouvez pas voter pour les propositions en dehors de votre ville ! Vous êtes redirigé vers la page de votre ville !"
+      redirect_to current_user.city
+    end
   end
 end
