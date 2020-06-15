027167c3f066b554e21b55f06a814624c8d05478
ouin85 <bouinten@yahoo.fr>
2020-06-15 12:29:00 +0200

    delete categories_controller change method

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
-    @proposals = @city.proposals.where(is_online: true).sort { |p1, p2| p2.votes_count <=> p1.votes_count }
     @categories = Category.all
+    @proposals = @city.proposals.where(is_online: true).sort { |p1, p2| p2.votes_count <=> p1.votes_count }
   end
 end
