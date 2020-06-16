28af11eaee8d533f6ccc6b85709447a46a001956
ouin85 <bouinten@yahoo.fr>
2020-06-16 16:18:06 +0200

    add optional: true for belongs_to :comment and :proposal

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :proposal
-  belongs_to :comment
+  belongs_to :proposal, optional: true
+  belongs_to :comment, optional: true
   has_many :comments
   has_many :likes
   
