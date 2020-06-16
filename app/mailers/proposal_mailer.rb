aefa827a217cb92259d4e12912e443837283cb7f
mathildy <mathilde.prouchandy@gmail.com>
2020-06-16 16:07:08 +0200

    mails content modified

--- app/mailers/proposal_mailer.rb
+++ app/mailers/proposal_mailer.rb
@@ -35,7 +35,14 @@ class ProposalMailer < ApplicationMailer
     @user = proposal.user
     @votes = proposal.votes
     @url = 'https://agora-prd.herokuapp.com/'
-    
+    @url1 = 'https://www.monmandatlocal.fr/innover-pour-son-territoire/veux-inscrire-commune-demarche-de-democratie-participative-conseil-bien-faire/'
+    @url2 = 'https://www.modernisation.gouv.fr/sites/default/files/fichiers-attaches/boite-outils-demarches-participation.pdf'
+    @url3 = 'http://www.rij37.fr/je-monte-un-projet/comment-construire-mon-projet/'
+    @url4 = 'https://enrcit.fr/qu-est-ce-qu-un-projet-citoyen/'
+    @url5 = 'https://lumieresdelaville.net/citoyens-prochains-decideurs-ville/'
+    @url6 = 'https://www.amf.asso.fr/documents-modalites-participation-citoyens-aux-decisions-locales/7718'
+    @url7 = 'http://periurbain.cget.gouv.fr/sites/default/files/Dossier%20pre%CC%81sentation%20Formation%20Emergence%20juin%202016-Les%20Pionnie%CC%80res.pdf'
+
     if @proposal.votes_count == 100
     mail(to: @user.email, subject: 'Mise en relation !')
     end
