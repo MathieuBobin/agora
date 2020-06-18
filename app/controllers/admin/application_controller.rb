5ef61b53dc93d075f15841a3053e615f8b1d28ad
TimotheRuffino <timothe.ruffino@gmail.com>
2020-06-18 10:15:23 +0200

    configure admin dashboard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,9 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      if !current_user.admin?
+        redirect_to root_path, alert: "Désolé vous n'avez pas l'accès !"
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
