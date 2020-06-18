d2d4ee4837af7507c258d83510bedd4d4f241240
ouin85 <bouinten@yahoo.fr>
2020-06-18 10:02:59 +0200

    fix bug ajax

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -1,8 +1,8 @@
 module ProposalsHelper
   def tweet(proposal)
     title = @proposal.title
-    first_name = current_user.first_name
-    last_name = current_user.last_name
+    first_name = @proposal.user.first_name
+    last_name = @proposal.user.last_name
     tweet = "#projet_agora Monsieur #{first_name} #{last_name} à proposé une initiative citoyenne dont le titre est : #{title}" 
     TwitterBot.new.tweet(tweet)
   end
