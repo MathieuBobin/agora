1f11b5d049a1b8b640820cb920de1723414c1c04
ouin85 <bouinten@yahoo.fr>
2020-06-19 16:27:28 +0200

    correct tweet message

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -3,7 +3,7 @@ module ProposalsHelper
     title = @proposal.title
     first_name = @proposal.user.first_name
     last_name = @proposal.user.last_name
-    tweet = "#projet_agora #{first_name} #{last_name} à proposé une initiative citoyenne dans sa ville dont le titre est : #{title}. Vous aussi ! vous pouvez proposer des initiatives citoyennes dans votre ville, cela est possible à travers notre site web ! https://agora-prd.herokuapp.com/" 
+    tweet = "#projet_agora #{first_name} #{last_name} a proposé une initiative citoyenne dans sa ville dont le titre est : #{title}. Vous aussi ! vous pouvez proposer des initiatives citoyennes dans votre ville, cela est possible à travers notre site web ! https://agora-prd.herokuapp.com/" 
     TwitterBot.new.tweet(tweet)
   end
 end
